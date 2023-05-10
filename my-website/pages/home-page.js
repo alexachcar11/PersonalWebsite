566321fa2b53055002eedbd280aa7ebab1866b1d
alexander-achcar <alexander.achcar@mail.mcgill.ca>
2023-05-10 17:34:12 -0400

    Home Page Template
    Added basic template for frontend.

--- my-website/pages/home-page.js
+++ my-website/pages/home-page.js
@@ -1,14 +1,37 @@
 import Link from 'next/link';
+import styles from '../styles/home-page.module.css';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
-export default function HomePage() { 
+function HomePage() { 
+
     return ( 
-        <>
-        <h1>Home Page</h1> 
-        <h2> 
-            <Link href="/"> Back to Home </Link>
-        </h2>
-        </>
+        
+        <div className={styles.container}>
+
+            <div className = {styles.title} > 
+                <div className={styles.imageWrapper}>
+                    <img className={styles.image} src="headshot.jpg" alt="Headshot" />
+                </div>
+                <h1>Alexander Achcar</h1>
+            </div>
+
+            <div className={styles.briefDesc}> 
+                <p> Hi! Welcome to my website! </p>
+                <p> Please feel free to explore around to learn more about either me personally or about some of the work I've done so far. </p>
+            </div>
+
+            <div className = {styles.botButtons}> 
+                <button> About Me </button>
+                <button> My Experiences </button>
+                <button> My Resume </button>
+                <button> Contact Me </button>
+            </div> 
+        </div> 
+        
     )
 
 }
+
+export default HomePage;
 
