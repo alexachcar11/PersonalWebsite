98a24ef4dec8ea33c5ac591aaa7f0dedd4fba172
alexander-achcar <alexander.achcar@mail.mcgill.ca>
2023-05-15 19:00:35 -0400

    About Me Content
    Added Content for the About Me page  (still needs to be edited). Added CSS for all the text (completed fully).

--- my-website/pages/about-me.js
+++ my-website/pages/about-me.js
@@ -29,42 +29,85 @@ function AboutMe() {
 
             <div className={styles.blurb}> 
                 <p> 
-                    Big Blurb
+                    My Name is Alexander Achcar. I'm a Software Developer Analyst at Citi Canada. I recently completed my 
+                    undergraduate degree at McGill University where I attained a Bachelor of Science with a Major in Software 
+                    Engineering and a Minor in Neuroscience. I'm extremely passionate about building software that has a direct 
+                    impact on improving quality of life. I have a wide range of work experience that has granted me unique 
+                    perspectives which I leverage to succeed on any project that I work on. 
                 </p> 
             </div>
 
             <div>  {/* pictures and descriptions */}
                 
-                <div className={styles.pictures}> 
+                <div className={styles.highlights}> 
 
-                    <div className={styles.pic1}>
-                            
+                    <div className={styles.pic1container}>
+                        <div className={styles.pic1Title}> 
+                            <Text 
+                                h1 
+                                size={25} 
+                                css={{ 
+                                    textGradient: "45deg, #772E25 -20%, #C44536 50%",
+                                }}
+                                weight="bold"
+                            >
+                                Hong Kong
+                            </Text>
+                        </div>
+                        <img className={styles.pic1} src="hkpic.jpg" alt="hkpic" />
+                        <div className={styles.desc1}>
+                            <p> 
+                                I grew up and lived in Hong Kong for 17 years. My time in Asia has given me a great 
+                                appreciation for different Asian cultures that has strongly influenced the person I am today. 
+                            </p>
+                        </div>
                     </div>
 
-                    <div className={styles.pic2}> 
-                        
+                    <div className={styles.pic2container}> 
+                        <div className={styles.pic2Title}> 
+                            <Text 
+                                h1 
+                                size={25} 
+                                css={{ 
+                                    textGradient: "45deg, #772E25 -20%, #C44536 50%",
+                                }}
+                                weight="bold"
+                            >
+                                Montreal
+                            </Text>
+                        </div>
+                        <img className={styles.pic2} src="mtlpic.jpg" alt="mtlpic" />
+                        <div className={styles.desc2}> 
+                            <p> 
+                                At 17 years old, I moved to Montreal where I've lived for the past 5 years. 
+                                I studied at Marianopolis College (CEGEP) and McGill University. 
+                            </p>
+                        </div>
                     </div>
 
-                    <div className={styles.pic3}> 
-                        
+                    <div className={styles.pic3container}> 
+                        <div className={styles.pic3Title}> 
+                            <Text 
+                                h1 
+                                size={25} 
+                                css={{ 
+                                    textGradient: "45deg, #772E25 -20%, #C44536 50%",
+                                }}
+                                weight="bold"
+                            >
+                                Toronto
+                            </Text>
+                        </div>
+                        <img className={styles.pic3} src="topic.jpeg" alt="topic" />
+                        <div className={styles.desc3}> 
+                            <p> 
+                                I recently accepted an offer to work full-time in Mississauga, ON. I'm super 
+                                excited to begin the next chapter of my life in Toronto and can't wait to see 
+                                what's next.
+                            </p>   
+                        </div>
                     </div>
                     
-                </div>
-
-                <div className={styles.descriptions}> 
-
-                    <div className={styles.desc1}>
-                        
-                    </div>
-
-                    <div className={styles.desc2}> 
-                        
-                    </div>
-
-                    <div className={styles.desc3}> 
-                        
-                    </div>
-
                 </div>
 
             </div>
