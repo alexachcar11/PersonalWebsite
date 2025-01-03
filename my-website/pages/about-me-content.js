bc7795598067ca9ce930724ca3c5641f8e4596de
alexander-achcar <achcar.alexander@gmail.com>
2025-01-03 14:21:44 +0800

    Added Experiences Page
    Few other minor cleaning adjustments also

--- my-website/pages/about-me-content.js
+++ my-website/pages/about-me-content.js
@@ -7,20 +7,21 @@ import Image from 'next/image'
 export const AboutMeContent = () => (
   <Box css={{px: "$12", mt: "$8", textAlign:"center", "@xsMax": {px: "$10"}}}>
     <Text h1 className={styles.aboutMeTitle}> About Me </Text>
+    <Text h3 className={styles.hiThere}> Hi There! </Text>
     <Text className={styles.aboutMeText} size="$xl"> 
-      Hi there! My name is Alexander Achcar. I&#39;m a passionate lifelong learner when it comes to all things 
+      My name is Alexander Achcar. I&#39;m a passionate lifelong learner when it comes to all things 
       technology — currently most interested in the intersection of technology with finance, stemming from 
       my current job as a Software Analyst at Citibank. I spent my first 12 months at Citi working on a team 
       focused on developing and iterating on a bond pricing application. My current team primarily implements 
       financial controls for a subset of Citibank traders. I&#39;ve been in this role since July 2022, which is 
-      when I transitioned into my career from McGill University. At McGill I found a deep interest for 
+      when I transitioned into my career from McGill University. At McGill, I found a deep interest for 
       learning about new technologies while also pursuing more general curiosities in Neuroscience through my 
       minor. 
     </Text>
 
     <Text className={styles.aboutMeText} size="$xl"> 
-      I put together this website as a means to become familiar with a modern frontend tech stack and give a 
-      small introduction to me as a person. Please feel free to reach out through LinkedIn or email via the 
+      I put this website together as a means to become familiar with a modern frontend tech stack and give a 
+      small introduction to me as a person and developer. Please feel free to reach out through LinkedIn/Email via the 
       contact me tab in the top right. 
     </Text>
   </Box>
--- my-website/pages/experiences-content.js
+++ my-website/pages/experiences-content.js
@@ -1,12 +1,88 @@
 import styles from '../styles/experiences.module.css';
-import { Text, Button} from "@nextui-org/react"
+import { Text, Button, Card, Spacer} from "@nextui-org/react"
 import { Box } from "./box.js"
 import Image from 'next/image'
 
 export const ExperienceContent = () => (
 
   <Box css={{px: "$12", mt: "$8", textAlign:"center", "@xsMax": {px: "$10"}}}>
-    <Text h1> Experiences </Text>
+    <Text h1 className={styles.experiencesTitle}> Experiences </Text>
+      <div className={styles.fullGrid}> 
 
+        <div className={styles.citiFullTime}> 
+          <div className={styles.citiFullTimeLogoBox}> 
+            <Image 
+              src="/citilogo.png" 
+              width="400"
+              height="400"
+              alt="Citi Logo"
+            />
+          </div>
+          <div className={styles.citiFullTimeText}> 
+            <Text h2> Software Analyst </Text>
+            <Text h4> July 2023 - Current </Text>
+            <Text> Fundamental Review of Trading Book </Text>
+            <Spacer x={4} y={2} />      
+            <Text h2> Software Analyst </Text>
+            <Text h4> June 2022 - July 2023 </Text>
+            <Text> Local Markets Bond Pricer </Text>
+            <Spacer x={4} y={2} />      
+            <Text h2> Summer Software Analyst </Text>
+            <Text h4> May 2022 - August 2022 </Text>
+            <Text> Derivative Pricing (Business Analyst) </Text>    
+          </div>
+        </div>
+
+
+        <div className={styles.fca}> 
+          <div className={styles.fcaLogoBox}> 
+            <Image 
+              src="/fcalogo-modified.png" 
+              width="600"
+              height="600"
+              alt="FCA Logo"
+            />
+          </div>
+          <div className={styles.fcaText}> 
+            <Text h2> Coding/Stem Instructor </Text>
+            <Text h4> June 2021 - August 2021 </Text>
+          </div>
+        </div>
+
+
+        <div className={styles.ni2}> 
+          <div className={styles.ni2LogoBox}> 
+            <Image 
+              src="/ni2logo-modified.png" 
+              width="400"
+              height="400"
+              alt="Ni2 Logo"
+            />
+          </div>
+          <div className={styles.ni2Text}> 
+            <Text h2> Software Developer Intern </Text>
+            <Text h4> June 2019 - August 2019 </Text>
+            <Spacer x={4} y={2} />      
+            <Text h2> Software Developer Intern </Text>
+            <Text h4> June 2017 - August 2017 </Text>
+          </div>
+        </div>
+
+
+        <div className={styles.actel}> 
+          <div className={styles.actelLogoBox}> 
+            <Image 
+              src="/actellogo.png" 
+              width="300"
+              height="300"
+              alt="Actel Logo"
+            />
+          </div>
+          <div className={styles.actelText}> 
+            <Text h2> Research Intern </Text>
+            <Text h4> May 2020 - August 2020 </Text>
+          </div>
+        </div>
+      </div>
   </Box>
 );
